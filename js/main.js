fdea56f7f7bf38d430f3707ebda389b0eab20dcc
Michele <micheleierniero@gmail.com>
2021-07-22 17:23:27 +0200

    display none  e block ticket

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
-// stampa nome biglietto
+// stampa biglietto
 
 var btnGenera = document.getElementById("calcola");
 
 console.log(btnGenera);
 
+
 btnGenera.addEventListener('click',
     function() {
         var nome = document.getElementById('name').value;
         document.getElementById('nomePasseggero').innerHTML = nome;
+        var km = document.getElementById('km').value;
+        var age = document.getElementById('age').value;
+        var ticket = document.getElementById('ticket');
+        ticket.style.display = 'block'
     }
 )
+
+
+// scomparsa ticket
+
+var btnAnnulla = document.getElementById("annulla");
+
+console.log(btnAnnulla);
+
+btnAnnulla.addEventListener('click',
+    function() {
+        ticket.style.display = 'none'
+    }
+)
+
+
 
 
 
